5bfb8ab7fb5164a457ed7e5f5d34e10ebb83b430
Francisco Magana <fmagana@fhps.net>
2023-08-03 14:54:58 -0400

    Added Generics to make requests building easier

--- client.go
+++ client.go
@@ -8,10 +8,16 @@ import (
 	"time"
 )
 
-type IncidentIQ struct {
+type Site struct {
 	Domain string
-	SiteID string
+	Id     string
 	Token  string
+}
+
+type Options[T any] struct {
+	Method   string
+	Endpoint string
+	Response T
 }
 
 const url = "https://%s.incidentiq.com/api/v1.0%s"
@@ -20,29 +26,37 @@ var client = &http.Client{Timeout: 10 * time.Second}
 
 // The request in the function uses the POST method but we do
 // not provide a body to show that we are not expecting changes
-// to the endpoint. It is how the API works *eye roll*
-func (iiq IncidentIQ) doGet(endpoint string, model interface{}) error {
-	url := fmt.Sprintf(url, iiq.Domain, endpoint)
+// to the endpoint. It is how the API works *eye roll* '|' for bulk
+func do[T any](site Site, options Options[T]) (T, error) {
+	url := fmt.Sprintf(url, site.Domain, options.Endpoint)
 
-	request, err := http.NewRequest(http.MethodPost, url, nil)
+	request, err := http.NewRequest(options.Method, url, nil)
 	if err != nil {
-		return err
+		return options.Response, err
 	}
 
-	request.Header.Add("SiteId", iiq.SiteID)
-	request.Header.Add("Authorization", "Bearer "+iiq.Token)
+	request.Header.Add("SiteId", site.Id)
+	request.Header.Add("Authorization", "Bearer "+site.Token)
 	request.Header.Add("Client", "ApiClient")
 
 	response, err := client.Do(request)
 	if err != nil {
-		return nil
+		return options.Response, err
 	}
 	defer response.Body.Close()
 
 	body, err := io.ReadAll(response.Body)
 	if err != nil {
-		return err
+		return options.Response, err
 	}
 
-	return json.Unmarshal(body, &model)
+	return options.Response, json.Unmarshal(body, &options.Response)
+}
+
+func get[T any](site Site, endpoint string) (T, error) {
+	return do(site,
+		Options[T]{
+			Method:   http.MethodGet,
+			Endpoint: endpoint,
+		})
 }
--- client_test.go
+++ client_test.go
@@ -1,29 +1,16 @@
 package iiq
 
 import (
-	"os"
 	"testing"
 
 	"github.com/fmagana-fhps/incidentiq-api-go/models"
-	"github.com/joho/godotenv"
 )
 
-func TestClientResponses(t *testing.T) {
-	err := godotenv.Load(".env.local")
-	if err != nil {
-		t.Error(err)
-	}
+func TestClientFail(t *testing.T) {
+	model, _ := do[models.ItemResponse[models.Asset]](Site{"fhps", "token", "token"},
+		Options[models.ItemResponse[models.Asset]]{Method: "GET", Endpoint: ""})
 
-	site := &IncidentIQ{
-		SiteID: os.Getenv("SITEID"),
-		Token:  os.Getenv("TOKEN"),
-		Domain: os.Getenv("DOMAIN"),
-	}
-
-	model := models.ItemResponse{}
-	site.doGet("/assets?$s=1", &model)
-
-	if model.ItemCount != 1 {
-		t.Errorf("'/assets?$s=1' = %d; want 1", model.ItemCount)
+	if model.StatusCode != 500 {
+		t.Errorf("check = %d; want 500", model.StatusCode)
 	}
 }
