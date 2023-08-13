c197c6f64262a513e5485bf5ba0d36dca2f835e8
fmagana <fmagana@fhps.local>
2023-08-13 14:44:22 -0400

    updated get asset by asset tags with parameters

--- tests/assets_test.go
+++ tests/assets_test.go
@@ -32,12 +32,18 @@ func TestAssetById(t *testing.T) {
 }
 
 func TestAssetByAssetTag(t *testing.T) {
-	assetTags := []string{"82888", "82889"}
-	result, err := client.AssetsByAssetTag(assetTags...)
+	assetTags := []string{"41708", "43023", "43914", "43921", "43922", "43923",
+		"44259", "45014", "45017", "45020", "45027", "45037", "45075", "45408",
+		"45861", "82880", "82881", "82882", "82883", "82884", "82885", "82886",
+		"82887", "82888", "82889",
+	}
+
+	params := iiq.Parameters{PageSize: len(assetTags)}
+	result, err := client.AssetsByAssetTag(params, assetTags...)
 	if err != nil {
 		t.Error(err)
 	}
-	for i := range result {
+	for i := 0; i < len(assetTags); i++ {
 		if result[i].AssetTag != assetTags[i] {
 			t.Errorf("AssetTag = %s; expected %s", result[i].AssetTag, assetTags[i])
 		}
